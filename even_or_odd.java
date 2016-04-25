399ce3bb552f492b62c1e53ca6f4d5ce1466ca01
Grzegorz Witczak <grzesiekwitczak@gmail.com>
2016-04-25 19:23:39 +0200

    Write a program to check if a number is even or odd

---
+++ even_or_odd.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class ex1 {
+	
+	public static void main(String[] args) {
+	
+	int num = 3;
+	if(num % 2 == 0){
+		System.out.println("Number: " +num + " is even");
+	}else{
+		System.out.println("Number: " +num + " is odd");
+	}
+  }
+}
